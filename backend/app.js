c067b217ec952a20842ad2ba3b03a4038b168fc9
Yulia Markowa <markowayulia@yandex.ru>
2021-07-07 14:53:55 +0300

    back

--- backend/app.js
+++ backend/app.js
@@ -39,14 +39,16 @@ app.get('/crash-test', () => {
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
+    password: Joi.string().required().min(6).pattern(/^[a-zA-Z0-9]{6,}$/)
+      .trim(),
   }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
+    password: Joi.string().required().min(6).pattern(/^[a-zA-Z0-9]{6,}$/)
+      .trim(),
   }),
 }), createUser);
 
