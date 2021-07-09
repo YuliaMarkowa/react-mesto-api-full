e46bd502098f2ed8c8685de2189c338d236d3c8a
Yulia Markowa <markowayulia@yandex.ru>
2021-07-09 19:00:37 +0300

    ws

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
 
 const { celebrate, Joi, errors } = require('celebrate');
@@ -15,7 +14,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const NotFoundError = require('./errors/not-found-err');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3005 } = process.env;
 const app = express();
 
 const allowedCors = [
@@ -39,8 +38,6 @@ app.use((req, res, next) => {
 
   next();
 });
-
-app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
