98be00e43a5b690b4e3bfa676f234169c3e4b03a
Yulia Markowa <markowayulia@yandex.ru>
2021-07-09 17:35:37 +0300

    webs

--- backend/app.js
+++ backend/app.js
@@ -23,9 +23,23 @@ const allowedCors = [
   'http://yulia.students.nomoredomains.club',
 ];
 
-app.use(cors({
-  origin: allowedCors,
-}));
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+  }
+  res.status(200).send();
+  next();
+});
+
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
