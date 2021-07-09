2f7279a04cce853971e375b3b5bda9e209ce1743
Yulia Markowa <markowayulia@yandex.ru>
2021-07-09 17:38:56 +0300

    ws

--- backend/app.js
+++ backend/app.js
@@ -34,8 +34,9 @@ app.use((req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.status(200).send();
   }
-  res.status(200).send();
+
   next();
 });
 
